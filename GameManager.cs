c8f47729d6af5acd6e5e616ec23bafc92c50a4fe
ricearoni68 <sclement2@gmail.com>
2023-11-17 15:14:44 -0800

    Update Gamemanager to improve OpenAI flow

--- GameManager.cs
+++ GameManager.cs
@@ -10,12 +10,27 @@ public partial class GameManager : Node
     private readonly SecretManager secretManager;
     private String _ChatGPTAIKey;
 
+    // Chat model info
     private readonly String _apiURL = "https://api.openai.com/v1/chat/completions";
-    private float _temperature = 0.5f;
-    private int _maxTokens = 1024;
+    [Export] private float _temperature = 0.5f;
+    [Export] private int _maxTokens = 1024;
+    [Export] private String _model = "gpt-3.5-turbo";
     private String[] _headers;
-    private String _model = "gpt-3.5-turbo";
 
+    // Get the NPC's name.
+    [Export] string npcName = "Villager";
+
+    // Get the player's dialogue.
+    [Export] string _playerDialogue = "Explain the Godot engine in 20 words";
+
+    // Dialogue rules
+    [Export]
+    string dialogueRules = "You are in a fantasy RPG engaging me in conversation. " +
+                "Reply to my questions and never break character. Don't mention that you are an AI or that this is a game. " +
+                "Only respond with your dialogue text and nothing else, but you can imply actions with []. " +
+                "For example: [laughs].";
+
+    //private List<OpenAIDialogueMessage> _conversation = new();
     private List<Dictionary<string, string>> _conversation = new();
     private HttpRequest _request;
 
@@ -35,23 +50,23 @@ public partial class GameManager : Node
     {
         _ChatGPTAIKey = GetSecret();
         _headers = new String[] { "Content-type: application/json", "Authorization: Bearer " + _ChatGPTAIKey };
-        GD.Print("Secret: " + _ChatGPTAIKey);
+        //GD.Print("Secret: " + _ChatGPTAIKey + "\n");
 
         //Create an HTTP request node and connect its completion signal.
         _request = new HttpRequest();
         AddChild(_request);
         _request.Connect("request_completed", new Callable(this, "OnRequestCompleted"));
 
-        DialogueRequest("Explain the Godot engine in 20 words");
+        DialogueRequest(_playerDialogue);
     }
 
     private void OnRequestCompleted(long result, long responseCode, string[] headers, byte[] body)
     {
-        GD.Print("Inside 'OnRequestCompleted' method");
+        //GD.Print("Inside 'OnRequestCompleted' method");
 
         if (responseCode == 200)
         {
-            GD.Print("Got response");
+            GD.Print("Got response from OpenAI \n");
             // Handle the response body or any other logic
             string responseBody = System.Text.Encoding.UTF8.GetString(body);
             JsonDocument jsonDoc = JsonDocument.Parse(responseBody);
@@ -74,16 +89,40 @@ public partial class GameManager : Node
 
     public void DialogueRequest(string playerDialogue)
     {
-        GD.Print($"{playerDialogue}");
+        string prompt = playerDialogue;
 
+        if (_conversation.Count == 0)
+        {
+            // CurrentNPC.physicalDescription 
+            // CurrentNPC.personality
+            // CurrentNPC.locationDescription
+            // CurrentNPC.SecretKnowledge
+            string physicalDescription = "An old wizard with a purple robe and long white beard.";
+            string personality = "Wise, smart, mysterious.";
+            string locationDescription = "Walking along a path in the village.";
+            string secretKnowledge = "You know the password to gain access to the Knight's house. It is the word Please.";
+
+            string headerPrompt = "Act as a " + physicalDescription + " in a fantasy RPG. ";
+
+            headerPrompt += "As a character, you are " + personality + ". ";
+            headerPrompt += "Your location is " + locationDescription + ". ";
+            headerPrompt += "You have secret knowledge that you will not speak about unless asked by me: " + secretKnowledge + ". ";
+
+            prompt = dialogueRules + "\n" + headerPrompt + "\nWhat is your first line of dialogue?";
+        }
+
+
+        GD.Print("Prompt sent to OpenAI: " + prompt + "\n");
+
+        // add a new message to the array
+        //_conversation.Add(new OpenAIDialogueMessage { Role = "user", Content = prompt });
         _conversation.Add(new Dictionary<string, string>
             {
                 { "role", "user" },
-                { "content", playerDialogue }
+                { "content", prompt }
             });
 
-        GD.Print($"Content: {_conversation[0]["content"]}");
-
+        // create the request body
         Dictionary<string, object> postData = new Dictionary<string, object>
         {
             { "messages", _conversation },
@@ -94,10 +133,14 @@ public partial class GameManager : Node
 
         string postDataJson = JsonConvert.SerializeObject(postData);
 
+        //GD.Print($"postDataJson: {postDataJson}");
 
-        GD.Print($"postDataJson: {postDataJson}");
+        Error sendRequest = _request.Request(_apiURL, _headers, HttpClient.Method.Post, postDataJson);
 
-        _request.Request(_apiURL, _headers, Godot.HttpClient.Method.Post, postDataJson);
-
+        // if there was a problem, make it known
+        if (sendRequest != Error.Ok)
+        {
+            GD.Print("There was an error!");
+        }
     }
 }
